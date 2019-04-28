27f96c84f5408b815c8ef36e0946ac22b427f3ab
Paul.Wen <Paul.Wen@feixitek.com>
2019-04-28 10:37:12 +0800

    creat DB_test.py

---
+++ DB_test.py
@@ -0,0 +1,14 @@
+# coding:utf-8
+
+def DB_test():
+    haap_db_opration = DB.HAAP()
+    haap_db_opration.insert()
+
+    SW_db_opration = DB.SW()
+    SW_db_opration.insert()
+    
+    
+    
+    
+    return 
+
