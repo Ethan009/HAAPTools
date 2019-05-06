b18d93f6feaca7cc7bc9b388ab30147224778cfa
Paul.Wen <Paul.Wen@feixitek.com>
2019-05-06 14:13:06 +0800

    paul optimise_newDB,DB.py,insert,update

--- DB.py
+++ DB.py
@@ -7,7 +7,7 @@ import datetime
 import Source as s
 import SANSW as sw
 import HAAP as haap
-
+from posix import lstat
 
 # read config and connet to the datebase
 cfgDB = gc.DBConfig()
@@ -19,13 +19,13 @@ intDBPort = cfgDB.port()
 print("strDBName:", intDBPort)
 
 # <<<Get Config Field>>>
-#HAAP
+# HAAP
 haapcfg = gc.EngineConfig()
 list_engines_IP = haapcfg.list_engines_IP()[0]
 telnet_port = haapcfg.telnet_port()
 FTP_port = haapcfg.FTP_port()
 passwd = haapcfg.password()
-#switch
+# switch
 swcfg = gc.SwitchConfig()
 list_sw_IP = swcfg.list_switch_IP()
 list_sw_alias = swcfg.list_switch_alias()
@@ -34,51 +34,55 @@ user = swcfg.username()
 passwd = swcfg.password()
 list_sw_ports = swcfg.list_switch_ports()
 
-
 connect(strDBName, host=strDBHost, port=intDBPort)
 
 # intialize 3 collections
 
-def _HAAP():
-    return haap.Status(list_engines_IP, telnet_port,
-                       passwd, FTP_port)
+#HAAP
+def haap_insert(self):
+    HAAP().insert(n, lstSTS)
 
-def _SW():
-    return sw.Status(list_sw_IP, ssh_port,
-                     user, passwd, list_sw_ports)
-    
+def is_there_HAAP():
+    dbHAAP = HAAP()
+    list_all_HAAP = dbHAAP.get_all_HAAP_status()
+    return list_all_HAAP
 
-def get_from_haap_status():
-    '''
-    @author: paul
-    @note: 获取引擎数据
-    '''
-    status = _HAAP()
-    status = status.xxxxxxx()
-    print("status.update_sss:",status)
-    return status
+#SANSW
+def switch_insert():
+    SANSW().insert(n, origin, switch_summary, switch_status)
 
-def get_from_switch_status():
-    '''
-    @note: 获取交换机数据
-    '''
-    status = _SW()
-    status = status.xxxxxxx()
-    print("status.update_sss:",status)
-    return status
+def is_there_switch():
+    dbswitch = SANSW()
+    list_all_switch = dbswitch.get_all_switch()
+    return list_all_switch
 
+#Warning 
+def insert_warning():
+    Warning().insert(n, level, warn_message, confirm_status)
 
+def update_warning():
+    Warning().update_Warning()
 
+def is_there_unconfirm_warning():
+    dbWarning = Warning()
+    unconfirm_warnning = dbWarning.get_all_unconfirm_warning()
+    if unconfirm_warnning:
+        lstAllUCW = []
+        for record in unconfirm_warnning:
+            lstAllUCW.append([record.time, record.level, record.warn_message])
+            
 
 class collHAAP(Document):
     time = DateTimeField(default=datetime.datetime.now())
     engine_status = DictField()
+
 
 class collSANSW(Document):
     time = DateTimeField(default=datetime.datetime.now())
     origin = DictField()
     switch_summary = DictField()
     switch_status = DictField()
+
 
 class collWarning(Document):
     time = DateTimeField(default=datetime.datetime.now())
@@ -89,6 +93,7 @@ class collWarning(Document):
 
 # DB opration of HAAP
 class HAAP(object):
+    
     # def __init__(self):
     #     self.last_record_list = get_last_record_list()
     #     self.time_engine_last_record = self.last_record_list[0]
@@ -102,11 +107,11 @@ class HAAP(object):
         collHAAP.objects(date__gte=time_start,
                          date__lt=time_end).order_by('-date')
 
-    def query_all(self):
-        return collHAAP.objects().order_by('-time')
-
-    def query_N_records(self, intN):
-        return collHAAP.objects().order_by('-time').limit(intN)
+#     def query_all(self):
+#         return collHAAP.objects().order_by('-time')
+# 
+#     def query_N_records(self, intN):
+#         return collHAAP.objects().order_by('-time').limit(intN)
 
     def query_last_record(self):
         return collHAAP.objects().order_by('-time').first()
@@ -114,21 +119,11 @@ class HAAP(object):
     def query_time_of_first_records(self):
         return collHAAP.objects().order_by('-time').first()
 
-    def get_last_record_list(self):
+    def get_all_HAAP_status(self):
         last_record = self.query_last_record()
-        if last_record:
-            record_time = last_record.time
-            lstHAAPstatus = last_record.engine_status
-            print("lstHAAPstatus:",lstHAAPstatus)
-            return record_time, lstHAAPstatus
-        else:
-            return
-
-    def time_engine_last_record():
-        pass
-
-    def status_engine_last_record():
-        pass
+        lst_all_status = [last_record.time,last_record.engine_status]
+        print(lst_all_status)
+        return lst_all_status
 
 
 class SANSW(object):
@@ -232,47 +227,39 @@ class SANSW(object):
     def query_range(self, time_start, time_end):
         collSANSW.objects(date__gte=time_start,
                           date__lt=time_end).order_by('-date')
-
-    def query_all(self):
-        return collSANSW.objects().order_by('-time')
-
-    def query_N_records(self, intN):
-        return collSANSW.objects().order_by('-time').limit(intN)
+# 
+#     def query_all(self):
+#         return collSANSW.objects().order_by('-time')
+# 
+#     def query_N_records(self, intN):
+#         return collSANSW.objects().order_by('-time').limit(intN)
 
     def query_first_records(self):
         return collSANSW.objects().order_by('-time').first()
    
-    def get_last_record_list(self):
-        '''
-        @note: 获取交换机数据
-        '''
+    def get_all_switch(self):
         last_record = self.query_first_records()
-        if last_record:
-            record_time = last_record.time
-            lst_switch_origin = last_record.origin
-            lst_switch_summary = last_record.switch_summary
-            lst_switch_status = last_record.switch_status
-            lstall = [record_time, lst_switch_origin, lst_switch_summary, lst_switch_status]
-            return lstall
-        else:
-            return
-
+        lstall = [last_record.time, last_record.origin,
+                last_record.switch_summary, last_record.switch_status]
+        return lstall
+    
 
 class Warning(object):
-
+    
     def insert(self, time_now, lstdj, lstSTS, confirm):
         t = collWarning(time=time_now, level=lstdj,
-                     warn_message=lstSTS, confirm_status=confirm)
+                        warn_message=lstSTS, confirm_status=confirm)
         t.save()
-   
+    
     def query_range(self, time_start, time_end):
         collWARN.objects(date__gte=time_start,
-                         date__lt=time_end).order_by('-date')
-    def query_all(self):
-        return collWARN.objects().order_by('-time')
-
-    def query_N_records(self, intN):
-        return collWARN.objects().order_by('-time').limit(intN)
+                        date__lt=time_end).order_by('-date')
+# 
+#     def query_all(self):
+#         return collWARN.objects().order_by('-time')
+# 
+#     def query_N_records(self, intN):
+#         return collWARN.objects().order_by('-time').limit(intN)
 
     def query_first_records(self, intN):
         return collWARN.objects().order_by('-time').first()
@@ -280,51 +267,21 @@ class Warning(object):
     def update(self, intN):
         return collWARN.objects().order_by('-time').first()
     
-    def get_recond(self):
-        '''
-        @note: 获取confirm_status为o的数据
-        '''
-        warns = []
-        a = collWarning.objects(confirm_status=0)
-        for z in a:
-            warns.append({'level':z.level, 'time':z.time, 'message':z.warn_message})
-        return(warns)
+    def get_all_unconfirm_warning(self):
+        warning_status = collWarning.objects(confirm_status=0)
+        return warning_status
     
     def update_Warning(self):
-        '''
-        更新confirm_status  0 到 1
-        '''
-        update_Warning = collWarning.objects(confirm_status=0).update(confirm_status=1)
-    
-    def get_last_record_list(self):
-        '''
-        @note: 获取预警数据库信息
-        '''
-        last_record = self.get_recond()
-        if last_record:
-            lst_last_update = last_record
-            return lst_last_update
-        else:
-            return
-    
-    
-#####插入数据库
-class insert_all(object):
-    '''
-    @author: paul
-    @note: 插入数据库语句
-    '''
-    def switch_insert(self):
-        SANSW().insert(n, origin, switch_summary, switch_status)
-    
-    def haap_insert(self):
-        HAAP().insert(n, lstSTS)
-     
-    def warning_insert(self):
-        Warning().insert(n, level, warn_message, confirm_status)
+        return collWarning.objects(confirm_status=0).update(confirm_status=1)
 
 
 if __name__ == '__main__':
-    get_from_switch_status()
+    # SANSW().get_all_switch()
+  #  print(is_there_switch()[1])
+
+    #is_there_HAAP()
+    
+    
+    print("ok")
     pass
 
